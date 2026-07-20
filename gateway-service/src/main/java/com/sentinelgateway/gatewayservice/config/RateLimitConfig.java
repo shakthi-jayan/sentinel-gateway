55b4279cdd382ad37afb59f26338a358e92735a8
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-07-20 13:21:57 +0530

    Add Redis-backed token bucket rate limiter with 5-capacity, 10s expiration

--- gateway-service/src/main/java/com/sentinelgateway/gatewayservice/config/RateLimitConfig.java
+++ gateway-service/src/main/java/com/sentinelgateway/gatewayservice/config/RateLimitConfig.java
@@ -1,6 +1,16 @@
 package com.sentinelgateway.gatewayservice.config;
 
-import org.springframework.context.annotation.*;
+import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.AsyncProxyManager;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
@@ -13,6 +23,21 @@ import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFuncti
 
 @Configuration
 public class RateLimitConfig {
+
+    @Bean
+    public AsyncProxyManager<String> asyncProxyManager() {
+        RedisClient redisClient = RedisClient.create(
+                RedisURI.builder().withHost("localhost").withPort(6379).build());
+
+        StatefulRedisConnection<String, byte[]> connection =
+                redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+
+        return LettuceBasedProxyManager.builderFor(connection)
+                .withExpirationStrategy(
+                        ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofSeconds(10)))
+                .build()
+                .asAsync();
+    }
 
     @Bean
     public RouterFunction<ServerResponse> rateLimitedBackendRoute() {
